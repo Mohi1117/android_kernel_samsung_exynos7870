2bd09d1bb6d2cbcfa0600a0dd7385ed97bda76f1
Yunlei He <heyunlei@huawei.com>
2018-11-06 10:25:29 +0800

    f2fs: move dir data flush to write checkpoint process
    [ Upstream commit b61ac5b720146c619c7cdf17eff2551b934399e5 ]
    
    This patch move dir data flush to write checkpoint process, by
    doing this, it may reduce some time for dir fsync.
    
    pre:
    	-f2fs_do_sync_file enter
    		-file_write_and_wait_range  <- flush & wait
    		-write_checkpoint
    			-do_checkpoint	    <- wait all
    	-f2fs_do_sync_file exit
    
    now:
    	-f2fs_do_sync_file enter
    		-write_checkpoint
    			-block_operations   <- flush dir & no wait
    			-do_checkpoint	    <- wait all
    	-f2fs_do_sync_file exit
    
    Signed-off-by: Yunlei He <heyunlei@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -220,8 +220,8 @@ static int f2fs_do_sync_file(struct file *file, loff_t start, loff_t end,
 
 	trace_f2fs_sync_file_enter(inode);
 
-	sbi->sec_stat.fsync_count++;
-	sbi->sec_stat.fsync_dirty_pages += get_dirty_pages(inode);
+	if (S_ISDIR(inode->i_mode))
+		goto go_write;
 
 	/* if fdatasync is triggered, let's do in-place-update */
 	if (datasync || get_dirty_pages(inode) <= SM_I(sbi)->min_fsync_blocks)
